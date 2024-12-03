2f479262444220dbeefa6e399bea5a5e91fce8a5
Sandhya Chimpiri <sandhyacreddy2002@gmail.com>
2024-12-03 18:56:29 +0530

    web scraper

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
+    if (!origin || allowedOrigins.includes(origin)) {      
+      callback(null, true);                               //allow requests from origin
     } else {
       callback(new Error("Not allowed by CORS"));
     }
