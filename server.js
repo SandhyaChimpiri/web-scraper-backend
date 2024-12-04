dc08c6b04b3fe05eaed498c7a08408669b01f593
Sandhya Chimpiri <sandhyacreddy2002@gmail.com>
2024-12-04 11:37:35 +0530

    server code

--- server.js
+++ server.js
@@ -43,11 +43,9 @@ app.post("/scrape", async (req, res) => {
   let browser;
   try {
     console.log("Launching Puppeteer...");
-    console.log('Chromium path:', process.env.CHROMIUM_PATH);
     browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
-      executablePath: process.env.CHROMIUM_PATH || "C:/Program Files/Google/Chrome/Application/chrome.exe", 
     });
     console.log("Puppeteer cache directory:", process.env.PUPPETEER_CACHE_DIR);
 
