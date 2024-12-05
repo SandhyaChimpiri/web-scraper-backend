23b8cede803202dde08c4ec2e0f49ce34e12f406
Sandhya Chimpiri <sandhyacreddy2002@gmail.com>
2024-12-05 21:09:12 +0530

    server

--- server.js
+++ server.js
@@ -49,12 +49,10 @@ app.post("/scrape", async (req, res) => {
   let browser;
   try {
     browser = await puppeteer.launch({
-  args: chromium.args,
-  defaultViewport: chromium.defaultViewport,
-  executablePath:await chromium.executablePath ,// In production (Render) use chrome-aws-lambda's path
-  headless: chromium.headless,
-});
-console.log("Chromium executable path:", chromium.executablePath);
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
 
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
